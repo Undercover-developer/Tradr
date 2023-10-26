c83f3a17727d45d12878c03c73444aefd958f40c
Koleoso oyindamola <87513130+Undercover-developer@users.noreply.github.com>
2023-10-26 00:33:38 -0500

    update binance broker

--- Tradr Services/Binance Service/broker.js
+++ Tradr Services/Binance Service/broker.js
@@ -1,7 +1,7 @@
 const Binance = require('node-binance-api');
 const binance = new Binance().options({
-  APIKEY: '46697762e7558c6a53c5131b18ccef94f5850b818c046fb97b31532e7a4046ea',
-  APISECRET: 'c8faaca9a75e5efaa4774a8bda81d52daad2692a81ed4b9e122e3acaedeb9b5d',
+  APIKEY: '',
+  APISECRET: '',
   useServerTime: true,
   test:true
 });
